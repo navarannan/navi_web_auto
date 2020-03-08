4aa1bef5b64c6b06dd0dcfdf43c32e4ff9fb7089
navi <navarose.haneefa@cottonon.com.au>
2020-03-09 01:33:06 +1100

    add

---
+++ momento_api_automation/spec/airvisual_api_tests.rb
@@ -0,0 +1,16 @@
+require 'rest-client'
+require 'json'
+require 'jsonpath'
+require 'rspec'
+require 'date'
+
+
+describe "api tests for AirVisual" do
+
+  it 'should return 403 if no auth token, 200 if correct provided' do
+    resp_code = AirVisual.verify_api_authorization
+    puts resp_code
+    expect resp_code.to eq("200")
+  end
+
+end
---
+++ momento_api_automation/spec/support/airvisual_api.rb
@@ -0,0 +1,25 @@
+require 'rest-client'
+require 'json'
+require 'date'
+
+module AirVisual
+
+  @url = "api.airvisual.com/v2/"
+  @api_key = "89ebc6f3-a877-4207-9528-51b1bf33bb5e"
+
+  # @header_full_auth = {:authorization=> "Basic YXBwOjExMTE=", :accept => "application/json",:content_type => "application/json"}
+  # @header_basic_auth = {:authorization=> "Basic YXBwOjExMTE="}
+  # @random_no = Time.now.nsec.to_s
+  # @loyalty_email = "cottononqa" + "#{@random_no}" + "@gmail.com"
+  # @different_email = "cottonon" + "#{@random_no}" + "@gmail.com"
+  # @dob = 20.years.ago(Date.today).strftime("%m-%d-%Y")
+
+
+  def verify_api_authorization
+
+    get_rest_url = "#{@url}" + "countries?key=" + "#{@api_key}"
+    resp = RestClient.get(get_rest_url)
+    puts resp.code
+    return resp.code
+  end
+end
